ffe67d6f814ed224c281edbb58d7e7592721e085
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-06-20 00:40:52 -0500

    adding updated source code to repo

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+#role for pipeline
 resource "aws_iam_role" "ditf-cp-role" {
   name = "ditf-cp-role"
 
@@ -18,3 +19,80 @@ resource "aws_iam_role" "ditf-cp-role" {
 EOF
 
 }
+
+data "aws_iam_policy_document" "ditf-pipeline-policies" {
+    statement{
+        sid = ""
+        actions = ["codestar-connections:UseConnection"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+    statement{
+        sid = ""
+        actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+
+resource "aws_iam_policy" "ditf-pipeline-policy" {
+    name = "sitf-pipeline-policy"
+    path = "/"
+    description = "Pipeline policy"
+    policy = data.aws_iam_policy_document.ditf-pipeline-policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "ditf-pipeline-attachment" {
+    policy_arn = aws_iam_policy.ditf-pipeline-policy.arn
+    role = aws_iam_role.ditf-cp-role.id
+}
+
+
+#role for codebuild
+resource "aws_iam_role" "ditf-codebuild-role" {
+  name = "ditf-codebuild-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+data "aws_iam_policy_document" "ditf-build-policies" {
+    statement{
+        sid = ""
+        actions = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*","iam:*"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+
+resource "aws_iam_policy" "ditf-build-policy" {
+    name = "ditf-build-policy"
+    path = "/"
+    description = "Codebuild policy"
+    policy = data.aws_iam_policy_document.ditf-build-policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "ditf-codebuild-attachment1" {
+    policy_arn  = aws_iam_policy.ditf-build-policy.arn
+    role        = aws_iam_role.ditf-codebuild-role.id
+}
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-codebuild-attachment2" {
+    policy_arn  = "arn:aws:iam::aws:policy/PowerUserAccess"
+    role        = aws_iam_role.ditf-codebuild-role.id
+}
+
---
+++ pipeline.tf
@@ -0,0 +1,110 @@
+resource "aws_codebuild_project" "ditf-plan" {
+  name          = "ditf-plan"
+  description   = "Plan stage for terraform"
+  service_role  = aws_iam_role.ditf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:1.2.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/planspec.yml")
+ }
+}
+
+resource "aws_codebuild_project" "ditf-apply" {
+  name          = "ditf-apply"
+  description   = "Apply stage for terraform"
+  service_role  = aws_iam_role.ditf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.14.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential{
+        credential = var.dockerhub_credentials
+        credential_provider = "SECRETS_MANAGER"
+    }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/applyspec.yml")
+ }
+}
+
+
+resource "aws_codepipeline" "ditf_pipeline" {
+
+    name = "tf-cicd"
+    role_arn = aws_iam_role.ditf-cp-role.arn
+
+    artifact_store {
+        type="S3"
+        location = aws_s3_bucket.ditf_artifacts.id
+    }
+#coonnect github and pass the cource code
+    stage {
+        name = "Source"
+        action{
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeStarSourceConnection"
+            version = "1"
+            output_artifacts = ["tf-code"]
+            configuration = {
+                FullRepositoryId = "davoclock/aws-cicd-pipeline"
+                BranchName   = "master"
+                ConnectionArn = var.codestar_connector_credentials
+                OutputArtifactFormat = "CODE_ZIP"
+            }
+        }
+    }
+
+    stage {
+        name ="Plan"
+        action{
+            name = "Build"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "ditf-cicd-plan"
+            }
+        }
+    }
+
+    stage {
+        name ="Deploy"
+        action{
+            name = "Deploy"
+            category = "Build"
+            provider = "CodeBuild"
+            version = "1"
+            owner = "AWS"
+            input_artifacts = ["tf-code"]
+            configuration = {
+                ProjectName = "ditf-cicd-apply"
+            }
+        }
+    }
+
+}
