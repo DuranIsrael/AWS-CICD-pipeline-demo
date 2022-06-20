b86bc0606e771b6a75293a3772499e50e89ccdbd
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-06-20 00:55:30 -0500

    updated my code to pipeline.tf file

--- pipeline.tf
+++ pipeline.tf
@@ -69,7 +69,7 @@ resource "aws_codepipeline" "ditf_pipeline" {
       version          = "1"
       output_artifacts = ["tf-code"]
       configuration = {
-        FullRepositoryId     = "davoclock/aws-cicd-pipeline"
+        FullRepositoryId     = "duranisrael/AWS-CICD-pipeline-demo"
         BranchName           = "master"
         ConnectionArn        = var.codestar_connector_credentials
         OutputArtifactFormat = "CODE_ZIP"
