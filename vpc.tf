1c89537e16429a3f6e2ead863e5a66f9bcef1bc5
Duran Israel <98367725+DuranIsrael@users.noreply.github.com>
2022-06-20 01:09:50 -0500

    added subnet to the vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.2.0/16"
+resource "aws_vpc" "cicd_test" {
+  cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id     = aws_vpc.cicd_test.id
+    cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "cicd-subnet"
+  }
+}
+
+
